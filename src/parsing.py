5801c3e08a8686c05be5cac0ed66570868b71b56
Oualidelaz <gthb112000@gmail.com>
2026-04-23 18:01:09 +0000

    feat(parser): add validation for height, width, entry, and exit keys

--- src/parsing.py
+++ src/parsing.py
@@ -5,8 +5,55 @@ from pathlib import Path
 RED = "\033[0;31m"
 END = "\033[0m"
 
+
+def parser(key, value):
+    if key.lower() == "height" or key.lower() == "width":
+
+        try:
+            num = int(value)
+        except ValueError:
+            raise ValueError(f"Invalid {key.lower()} value: '{value}' must be an integer.")
+        
+        if num < 0:
+            raise ValueError(f"{key.capitalize()} cannot be negative, got {num}.")
+        if num >= 500:
+            raise ValueError(f"{key.capitalize()}: {num} is too large!")
+        return {key.upper(): num}
+
+
+    if key.lower() == "entry":
+        try:
+            x, y = value.split(',')
+        except ValueError:
+            raise ValueError("Value unpacking failed. ENTRY must be 'x,y' format")
+        try:
+            x = int(x)
+            y = int(y)
+        except ValueError:
+            raise ValueError(f"Invalid coordinates: x='{x}' and y='{y}' must both be integers.")
+        return {key.upper(): (x, y)}
+        
+
+    if key.lower() == "exit":
+        try:
+            x, y = value.split(',')
+        except ValueError:
+            raise ValueError("Value unpacking failed. EXIT must be 'x,y' format")
+        try:
+            x = int(x)
+            y = int(y)
+        except ValueError:
+            raise ValueError(f"Invalid coordinates: x='{x}' and y='{y}' must both be integers.")
+        return {key.upper(): (x, y)}
+        
+
+    raise ValueError(f"Unknown key: '{key}'.")
+
+
+
 try:
     if len(sys.argv) == 2:
+        result = dict()
         FILE = sys.argv[1]
         file_path = Path(FILE)
         if os.path.isfile(file_path) and FILE.endswith('.txt'):
@@ -24,6 +71,13 @@ try:
                             clean_line += character
                         if '=' not in clean_line:
                             raise ValueError(f"{RED}Invalid format: expected 'key=value'{END}")
+                        data = clean_line.split('=')
+                        try:
+                            key, value = data[0], data[1]
+                            if parser(key, value):
+                                result.update({key, value})
+                        except Exception:
+                            raise ValueError("config must contain key and value")
             except ValueError as e:
                 print(e)
                 sys.exit(1)
