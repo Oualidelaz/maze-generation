d84c5dc318b9ded01cba136b9c39eebed077c657
Oualidelaz <gthb112000@gmail.com>
2026-05-03 06:51:11 +0100

    fix: improve value parsing and update matrix generation

--- src/generate_matrix.py
+++ src/generate_matrix.py
@@ -1,5 +1,5 @@
 def generate_matrix(height, width):
     matrix = [[(x, y) for x in range(width)] for y in range(height)]
     return matrix
-]
+
 
--- src/parsing.py
+++ src/parsing.py
@@ -51,46 +51,50 @@ def parser(key, value):
 
 
 
-try:
-    if len(sys.argv) == 2:
-        result = dict()
-        FILE = sys.argv[1]
-        file_path = Path(FILE)
-        if os.path.isfile(file_path) and FILE.endswith('.txt'):
-            try:
-                with open(file_path, "r") as file:
-                    for line in file:
-                        stripped = line.strip()
-                        if not stripped or stripped.startswith("#"):
-                            continue
-                        ln = line.split('#', 1)[0].strip()
-                        clean_line = ""
-                        for character in ln:
-                            if character in (" ", "\t", "\v"):
+def parsing():
+    try:
+        if len(sys.argv) == 2:
+            result = dict()
+            FILE = sys.argv[1]
+            file_path = Path(FILE)
+            if os.path.isfile(file_path) and FILE.endswith('.txt'):
+                try:
+                    with open(file_path, "r") as file:
+                        for line in file:
+                            stripped = line.strip()
+                            if not stripped or stripped.startswith("#"):
                                 continue
-                            clean_line += character
-                        if '=' not in clean_line:
-                            raise ValueError(f"{RED}Invalid format: expected 'key=value'{END}")
-                        data = clean_line.split('=')
-                        try:
-                            key, value = data[0], data[1]
-                            if parser(key, value):
-                                result.update({key, value})
-                        except Exception:
-                            raise ValueError("config must contain key and value")
-            except ValueError as e:
-                print(e)
-                sys.exit(1)
-            except OSError as e:
-                print(f"{RED}File error: {e}{END}")
+                            ln = line.split('#', 1)[0].strip()
+                            clean_line = ""
+                            for character in ln:
+                                if character in (" ", "\t", "\v"):
+                                    continue
+                                clean_line += character
+                            if '=' not in clean_line:
+                                raise ValueError(f"{RED}Invalid format: expected 'key=value'{END}")
+                            data = clean_line.split('=')
+
+                            try:
+                                key, value = data[0], data[1]
+                                print(key, value)
+                                if parser(key, value):
+                                    result.update({key, value})
+                            except Exception:
+                                raise ValueError("config must contain key and value")
+                        return result
+                except ValueError as e:
+                    print(e)
+                    sys.exit(1)
+                except OSError as e:
+                    print(f"{RED}File error: {e}{END}")
+                    sys.exit(1)
+            else:
+                print(f"{RED}File not found or not a valid file: '{file_path}'{END}")
                 sys.exit(1)
         else:
-            print(f"{RED}File not found or not a valid file: '{file_path}'{END}")
+            print(f"{RED}Usage Example: <maze.py> <config.txt>{END}")
             sys.exit(1)
-    else:
-        print(f"{RED}Usage Example: <maze.py config.txt>{END}")
-        sys.exit(1)
 
-except Exception as e:
-    print(f"{RED}Unexpected error: {e}{END}")
-    sys.exit(1)
+    except Exception as e:
+        print(f"{RED}Unexpected error: {e}{END}")
+        sys.exit(1)
