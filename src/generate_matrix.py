4f8bdca5c174c2a64691d532e972217a99406bfc
Oualidelaz <gthb112000@gmail.com>
2026-05-03 14:47:13 +0100

    feat: generate matrix using Cell objects

--- src/generate_matrix.py
+++ src/generate_matrix.py
@@ -1,5 +1,10 @@
+from cell import Cell
+
 def generate_matrix(height, width):
-    matrix = [[(x, y) for x in range(width)] for y in range(height)]
-    return matrix
+    try:
+        matrix = [[Cell((x, y)) for x in range(width)] for y in range(height)]
+        return matrix
+    except Exception:
+        raise ValueError("Matrix generating Error!")
 
 
