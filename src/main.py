6554861837a03d747cf8030147540afd1a5a61a7
Oualidelaz <gthb112000@gmail.com>
2026-05-23 11:40:17 +0100

    Add maze UI menu, random color themes, and interactive maze controls

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+import random
+import time
 from parsing import parsing
 from generate_matrix import generate_matrix
 from dfs import dfs
@@ -5,22 +9,84 @@ from add_loops import add_loops
 from draw_maze import draw_maze
 from trace_u import trace_u
 
+
+def run(data, color):                
+    height = data.get("HEIGHT")
+    width = data.get("WIDTH")
+    entry = data.get("ENTRY")
+    exit = data.get("EXIT")
+    perfect = data.get("PERFECT")
+    matrix = generate_matrix(height, width)
+    current_color = color
+    trace_u(matrix, height, width, entry, exit)
+    dfs(matrix, entry, height, width)
+    if perfect:
+        add_loops(matrix, height, width)
+    start_cell = matrix[entry[1]][entry[0]]
+    exit_cell = matrix[exit[1]][exit[0]]
+    draw_maze(matrix, start_cell, exit_cell, current_color)
+
+
+def display_menu():
+    print("""
+==============================
+         MAZE MENU
+==============================
+
+[1] Draw Maze
+[2] Change Maze Color
+[3] Show Maze Path
+
+[0] Exit
+
+==============================""")
+
+
+
 def main():
+    RED = "\033[0;31m"
+    YELLOW = "\033[1;33m"
+    END = "\033[0m"
     try:
+        colors = ["CYAN", "BLUE", "YELLOW", "LIGHT_GRAY"]
+        previous_color = "white"
+        current_color = "white"
         data = parsing()
-        height = data.get("HEIGHT")
-        width = data.get("WIDTH")
-        entry = data.get("ENTRY")
-        exit = data.get("EXIT")
-        perfect = data.get("PERFECT")
-        matrix = generate_matrix(height, width)
-        trace_u(matrix, height, width, entry, exit)
-        dfs(matrix, entry, height, width)
-        if perfect:
-            add_loops(matrix, height, width)
-        start_cell = matrix[entry[1]][entry[0]]
-        exit_cell = matrix[exit[1]][exit[0]]
-        draw_maze(matrix, start_cell, exit_cell)
-    except Exception as e:
-        print(e)
+        os.system("cls") if os.name == "nt" else os.system("clear")
+        while True:
+            display_menu()
+            try:
+                choice = int(input("Choose an option: "))
+                if choice not in (0, 1, 2, 3):
+                    raise ValueError
+            except ValueError:
+                print(f"\n{RED}[!] Invalid choice!{END}")
+                time.sleep(0.5)
+
+            else:
+                if choice == 1:
+                    os.system("cls") if os.name == "nt" else os.system("clear")
+                    run(data, previous_color)
+                elif choice == 2:
+                    os.system("cls") if os.name == "nt" else os.system("clear")
+                    current_color = previous_color
+                    while current_color == previous_color:
+                        current_color = random.choice(colors)
+                    run(data, current_color)
+                    previous_color = current_color
+                elif choice == 3:
+                    os.system("cls") if os.name == "nt" else os.system("clear")
+                    run(data, current_color)
+                    # show path
+                elif choice == 0:
+                    print(f"\n{YELLOW}[*] Exiting Maze Program... Goodbye!{END}")
+                    sys.exit()
+
+
+    except KeyboardInterrupt:
+        print(f"{RED}[!] Interrupted by user. Exiting gracefully...{END}")
+        sys.exit()
+    except Exception:
+        print(f"{RED}[!] Something Wrong!{END}")
+        sys.exit()
 main()
